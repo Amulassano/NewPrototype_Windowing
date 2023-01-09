7489d1df70c7ad02dba6529bb6862af788195a4b
Amulassano <115482563+Amulassano@users.noreply.github.com>
2023-01-09 17:04:03 +0100

    Update Alterazionemain.c

--- Alterazionemain.c
+++ Alterazionemain.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <time.h>       // for clock_t, clock(), CLOCKS_PER_SEC
 #include "AlterazioneWindowing.h"
+#define file "micro100-10000.txt"
 
 int main(){
     FILE *f = fopen("input.txt","r");
     int e,ts;
+    FILE *ff = fopen(file,"w");
+    fprintf(ff,"SIZE_WINDOW: 100\n");
+    fprintf(ff,"SIZE_BUFFER: 10000\n");
+    fclose(ff);
     // per memorizzare il tempo di esecuzione del codice
     double time_spent = 0.0;
 
@@ -15,9 +20,9 @@ int main(){
         fscanf(f,"%d %d",&e,&ts);
         windowing(e,ts);
     }*/
-    for (int i = 2; i < 20; i=i+1) {
-        windowing(i,i);
 
+    for (int i = 1; i <= 5000; i++) {
+        windowing(i,i);
     }
 
 
@@ -26,8 +31,9 @@ int main(){
     // calcola il tempo trascorso trovando la differenza (end - begin) e
     // dividendo la differenza per CLOCKS_PER_SEC per convertire in secondi
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-
-    printf("The elapsed time is %f seconds", time_spent);
+    ff = fopen(file,"a");
+    fprintf(ff,"The elapsed time of the window function is %f seconds", time_spent);
+    fclose(ff);
 
 
     return 0;
